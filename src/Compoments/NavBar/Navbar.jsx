e387e313365cbe7d3c46ecaa1bb7169f6f3084a7
uzairsaddique <uzairsaddique@gmail.com>
2023-07-05 10:57:31 +0500

    updated the code

--- src/Compoments/NavBar/Navbar.jsx
+++ src/Compoments/NavBar/Navbar.jsx
@@ -30,7 +30,7 @@ Login &nbsp <AccountCircle/>
 </Button>
          ):(
 <Button color='inherit' component={Link} to={`/profile/:id`} className={classes.linkedButton} onClick={()=>{}}>
-{!isMoble && <>My Movies &nbsp </>}
+{!isMoble && <>My Movies &nbsp; </>}
 <Avatar style={{width: 30, height: 30}} alt='Profile' src='https://cdn.pixabay.com/photo/2017/02/23/13/05/avatar-2092113_1280.png'/>
 </Button>
          )}</div> 
